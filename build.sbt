f4fa843f13a8d5307dffcd4f61ca627b7862f51d
Ang, Hao Yang <haoyang.ang@sc.com>
2020-11-01 23:36:29 +0800

    temp

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
 addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full)
 
 libraryDependencies ++= Seq(
-  "co.fs2" %% "fs2-core" % "2.4.2"
+  "co.fs2" %% "fs2-core" % "2.4.4"
 )
 
 scalacOptions ++= Seq(
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,25 @@
-import cats.data.Chain
+import cats.data.{Chain, OptionT}
 import cats.implicits._
 import cats.effect.{ExitCode, IO, IOApp, Sync, Timer}
 import fs2.Stream
 
 import scala.concurrent.duration._
 import scala.util.chaining._
-import java.awt.{MouseInfo, Robot}
+import java.awt.{MouseInfo, PointerInfo, Robot}
+import java.awt.event.KeyEvent
 
 object Main extends IOApp {
   def schedule[F[_]: Timer](interval: FiniteDuration): Stream[F, Unit] = Stream.fixedDelay[F](interval)
 
-  def shakeMouse[F[_]](rbt: Robot)(implicit F: Sync[F]): F[Unit] = for {
-    ptr     <-  F suspend F.fromOption(Option(MouseInfo.getPointerInfo), new Error("No mouse."))
-    (x, y)  =   ptr.getLocation.pipe(p => (p.x, p.y))
-    _       <-  Chain(x + 1, x - 1, x).traverse(F delay rbt.mouseMove(_, y))
-  } yield ()
+  def safeGetPointerInfo[F[_]](implicit F: Sync[F]): OptionT[F, PointerInfo] = OptionT(F delay Option(MouseInfo.getPointerInfo))
+
+  def shakeMouseOrF15[F[_]](rbt: Robot)(implicit F: Sync[F]): F[Unit] = safeGetPointerInfo.handleError() match {
+    case Some(ptr) =>
+      val (x, y) = ptr.getLocation.pipe(p => (p.x, p.y))
+      Chain(x + 1, x - 1, x).traverse(F delay rbt.mouseMove(_, y)) *> F.delay(println(x, y))
+    case _ =>
+      (F delay rbt.keyPress(KeyEvent.VK_F15)) *> F.delay(println("key event"))
+  }
 
   override def run(args: List[String]): IO[ExitCode] = for {
     maybeInterval <-  args.headOption traverse { head =>
@@ -22,6 +27,6 @@ object Main extends IOApp {
     }
     interval      =   maybeInterval getOrElse 30.minutes
     rbt           <-  IO(new Robot)
-    _             <-  (schedule(interval) >> Stream.eval_(shakeMouse[IO](rbt))).compile.drain
+    _             <-  (schedule(interval) >> Stream.eval_(shakeMouseOrF15[IO](rbt))).compile.drain
   } yield ExitCode.Success
 }
